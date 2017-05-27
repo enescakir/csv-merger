7c13b3f85139aa0274af3877f687347113a13758
Enes Çakır <enes@cakir.web.tr>
2017-05-27 17:52:10 +0300

    Main logic added

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # CSV Merger
 
 `python3 merger.py --output output.csv --pattern unsubs`
+
 `python3 merger.py --output output.csv --directory /User/Desktop`
+
 `python3 merger.py --output output.csv --noheader`
--- merger.py
+++ merger.py
@@ -5,7 +5,7 @@ group = parser.add_mutually_exclusive_group()
 group.add_argument('-p', '--pattern', dest="pattern", action='store')
 parser.add_argument('-o', '--output', dest="output", action='store', required=True)
 parser.add_argument('-d', '--directory', dest="directory", action='store')
-parser.add_argument('-nh','--noheader', dest="header", action='store_true')
+parser.add_argument('-nh','--noheader', dest="noheader", action='store_true')
 arguments = parser.parse_args()
 
 pattern = ".*?"
@@ -15,14 +15,33 @@ if arguments.pattern:
     pattern = arguments.pattern
 
 if arguments.directory:
-    pattern = arguments.directory
+    directory = arguments.directory
 
-outputFileName = arguments.output
-csvFiles = []
+output = arguments.output
+csv_files = []
 
 curlist = os.listdir(directory)
 for name in curlist:
-    if name.endswith('.csv') and re.search(patterm, name):
+    if name.endswith('.csv') and re.search(pattern, name):
         newpath = directory + "/" + name
         if not os.path.isdir(newpath):
-            csvFiles.append(newpath)
+            csv_files.append(newpath)
+
+outputFile = open(output, 'w')
+writer = csv.writer(outputFile, delimiter=',');
+
+print("\nFound CSV File Count: " + str(len(csv_files)))
+
+for key, file in enumerate(csv_files):
+    reader = csv.reader(open(file))
+    rows = list(reader)
+    total_count = len(rows)
+    print("\t" + file[file.rfind("/")+1:] + " => " + str(total_count) + " rows")
+    if not arguments.noheader:
+        if(key == 0):
+            writer.writerow(rows.pop(0));
+    for row in rows:
+        writer.writerow(row);
+outputFile.close()
+total_count = len(list(csv.reader(open(output))))
+print("Output: " + output + " => " + str(total_count) + " rows\n")
